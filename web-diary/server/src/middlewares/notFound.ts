9f817267a2fcdb501816e454bc8f98cd4827a05d
Aditya Hansraj <hansrajaditya.me@gmail.com>
2025-08-21 22:37:24 +0530

    Delete web-diary directory

--- web-diary/server/src/middlewares/notFound.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export default (req: Request, res: Response, next: NextFunction) => {
-  res.status(404).json({
-    success: false,
-    error: 'Not Found',
-    data: null,
-  });
-};
--- web-diary/server/src/types/express.d.ts
+++
@@ -1,7 +0,0 @@
-import { UserType } from './types/UserType'; // Adjust the import path as needed
-
-declare global {
-  namespace Express {
-    interface User implements UserType {}
-  }
-}
